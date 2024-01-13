3f9bae4684e588a35fb9f21338c653f0a8dbfe24
LinZhuoChen <lin_zhuo_chen@163.com>
2024-01-13 13:56:38 +0800

    test pipline success!

--- pointrix/dataset/base_data.py
+++ pointrix/dataset/base_data.py
@@ -1,8 +1,10 @@
 import torch
+import numpy as np
+from PIL import Image
 from pathlib import Path
 from abc import abstractmethod
 from torch.utils.data import Dataset
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, asdict
 from typing import Any, Dict, Union, List
 
 from pointrix.camera.camera import Camera, TrainableCamera
@@ -54,45 +56,70 @@ class BaseReFormatData:
         raise NotImplementedError
 
 
-class BaseDataset(Dataset):
-    def __init__(self, config, format_data):
+## TODO: support cached dataset and lazy init
+class BaseImageDataset(Dataset):
+    def __init__(self, config, format_data: BaseDataFormat) -> None:
         self.config = config
         self.format_data = format_data
 
+    ## TODO: full init
     def __len__(self):
         return len(self.format_data)
 
     def __getitem__(self, idx):
-        pass
+        image_file_name, camera = self.format_data[idx]
+        image = self.load_image(image_file_name)
+        return {"image": image, 
+                "camera": asdict(camera)}
+    
+    def load_image(self, image_filename):
+        pil_image = Image.open(image_filename)
+        image = np.array(pil_image, dtype="uint8")  # shape is (h, w) or (h, w, 3 or 4)
+
+        if len(image.shape) == 2:
+            image = image[:, :, None].repeat(3, axis=2)
+        assert len(image.shape) == 3
+        assert image.dtype == np.uint8
+        assert image.shape[2] in [3, 4], f"Image shape of {image.shape} is in correct."
+        return image
 
 
 class BaseDataPipline:
-    def __init__(self, config):
+    def __init__(self, config, path: str):
         self._fully_initialized = False
         self.config = config
+        self.path = path
+
+        ## TODO: use registry
+        from pointrix.dataset.colmap_data import ColmapReFormat
+        self.train_format_data = ColmapReFormat(self.config, data_root=self.path, split="train").data_list
+        self.validation_format_data = ColmapReFormat(self.config, data_root=self.path, split="val").data_list
+
+        self.loaddata()
 
     # TODO use rigistry
-    @abstractmethod
     def get_training_dataset(self):
-        raise NotImplementedError
-
-    @abstractmethod
+        ## TODO: use registry
+        self.training_dataset = BaseImageDataset(config=self.config, format_data=self.train_format_data)
+        
     def get_validation_dataset(self):
-        raise NotImplementedError
+        self.validation_dataset = BaseImageDataset(config=self.config, format_data=self.validation_format_data)
 
-    @abstractmethod
     def loaddata(self):
+        self.get_training_dataset()
+        self.get_validation_dataset()
+
         self.training_loader = torch.utils.data.DataLoader(
             self.training_dataset,
-            batch_size=1,
-            shuffle=True,
+            batch_size=5,
+            shuffle=False,
             num_workers=1,
             pin_memory=True,
         )
         self.validation_loader = torch.utils.data.DataLoader(
             self.validation_dataset,
-            batch_size=1,
-            shuffle=True,
+            batch_size=5,
+            shuffle=False,
             num_workers=1,
             pin_memory=True,
         )
@@ -100,14 +127,15 @@ class BaseDataPipline:
         self.iter_train_image_dataloader = iter(self.training_loader)
         self.iter_val_image_dataloader = iter(self.validation_loader)
 
-    @abstractmethod
     def next_train(self, step=int):
-        raise NotImplementedError
+        try:
+            return next(self.iter_train_image_dataloader)
+        except:
+            self.iter_train_image_dataloader = iter(self.training_loader)
+            return next(self.iter_train_image_dataloader)
 
-    @abstractmethod
     def next_val(self, step=int):
-        raise NotImplementedError
+        return next(self.iter_val_image_dataloader)
 
-    @abstractmethod
     def get_param_groups(self):
         raise NotImplementedError
